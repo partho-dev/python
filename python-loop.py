3808f4befc8113a3f1b60e726499f44ad56abad2
Partho_Mac_15 <macbook@MacBooks-MacBook-Pro.local>
2025-02-11 12:00:49 +0530

    Python loop

---
+++ python-loop.py
@@ -0,0 +1,25 @@
+#print table like 5 * 1 = 5 
+
+# n = int(input("Please enter the table you want to print: "))
+
+# for i in range(1, 11):
+#     print(f"{n} * {i} = {n*i}"  )
+
+
+# Sum upto n numbers
+# n = int(input("Put the number: "))
+# sum = 0
+# for i in range(1, n+1):
+#     print(f"{sum}+{i}={sum+i}")
+#     sum = sum + i
+    
+# print(f"Total sum is: {sum}")
+
+# import module
+# import math 
+# sqr = math.sqrt(9)
+
+# print(sqr)
+
+from math import sqrt
+srt = print(sqrt(9))
